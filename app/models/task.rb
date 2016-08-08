27d29ecfaa649a1c3444d973f3a14f4929c461a2
khrystynaklochko <khrystynaklochko@gmail.com>
2016-08-08 19:55:03 +0200

    add dependencies

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+
+  belongs_to :user
+  self.per_page = 50
+
+  validates_presence_of :title
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_token
+  has_secure_password
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true
 end
