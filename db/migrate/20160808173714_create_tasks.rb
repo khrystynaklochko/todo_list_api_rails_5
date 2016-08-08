d5ebad4f725e2179aea56e1a832e08cd69e44a3e
khrystynaklochko <khrystynaklochko@gmail.com>
2016-08-08 19:44:16 +0200

    create db

--- db/migrate/20160808173714_create_tasks.rb
+++ db/migrate/20160808173714_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.string :description
-      t.string :user_id
+      t.string   :title
+      t.text     :description
+      t.integer  :user_id
 
       t.timestamps
     end
